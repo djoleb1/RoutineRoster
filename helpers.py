1040be77e91c095fdb81e576d1b581f37bd6e628
djoleb1 <djordje.brdar2202@gmail.com>
2023-12-10 23:34:02 +0100

    Adding esc chars

--- helpers.py
+++ helpers.py
@@ -4,11 +4,14 @@ from flask import Flask, render_template, redirect
 
 def apology(message, code=400):
     """Render message as an apology to user."""
-    
-    """
-    Escape special characters.
+    def escape(s):
+        """
+        Escape special characters.
 
-    https://github.com/jacebrowning/memegen#special-characters
-    """
-
-    return render_template("apology.html", top=code, bottom=message)
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code

fa2914a5f5ace9c8b1c9788e46135a0dffc22e76
djoleb1 <djordje.brdar2202@gmail.com>
2023-12-11 21:14:11 +0100

    Adding JS and hamburger menu

---
+++ static/script.js
@@ -0,0 +1,13 @@
+let hamburger = document.querySelector('.hamburger');
+let navbar = document.querySelector('.navbar');
+
+hamburger.addEventListener('click', function(){
+    hamburger.classList.toggle('active');
+    navbar.classList.toggle('active');
+})
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', function(){
+    hamburger.classList.remove('active');
+    navbar.classList.remove('active');
+}))
+
